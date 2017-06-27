198bfeed100f5535978b26ea7af6c9099b1ff7bd
Aaron Cameresi <aaroncameresi@gmail.com>
2017-06-27 09:17:54 -0400

    Update 1

---
+++ subtraction.rb
@@ -0,0 +1,14 @@
+
+
+#subtraction
+def subtraction
+
+		print "enter an integer."
+			a = gets.to_i
+		print "enter an integer to subtract.\n"
+			b = gets.to_i
+			c = a - b
+		print "#{a} minus #{b} equals #{c}.\n"
+end
+
+subtraction
