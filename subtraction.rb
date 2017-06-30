d74cbd3ff466b3679faeacc0ce50401085a7ab34
Aaron Cameresi <aaroncameresi@gmail.com>
2017-06-30 16:17:43 -0400

    update to pass numbers

--- subtraction.rb
+++ subtraction.rb
@@ -1,14 +1,22 @@
 
 
 #subtraction
-def subtraction
+# def subtraction
 
-		print "enter an integer."
-			a = gets.to_i
-		print "enter an integer to subtract.\n"
-			b = gets.to_i
-			c = a - b
-		print "#{a} minus #{b} equals #{c}.\n"
+# 		print "enter an integer."
+# 			a = gets.to_i
+# 		print "enter an integer to subtract.\n"
+# 			b = gets.to_i
+# 			c = a - b
+# 		print "#{a} minus #{b} equals #{c}.\n"
+# end
+
+# subtraction
+
+
+def subtraction(number1, number2)
+	total = number1 - number2
+	p total
 end
 
-subtraction
+subtraction(5, 3)
